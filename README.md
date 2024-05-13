767a0e6d672b219623ab69bc132fbbec9fb7978e
MasterCoder1323 <121037342+MasterCoder1323@users.noreply.github.com>
2024-05-13 22:53:06 +0000

    install deps executable

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# electron
+# Electron
 All my work with electron.
+## To build yourself
+On ubuntu foca fossa run ./install-deps.sh
---
+++ install-deps.sh
@@ -0,0 +1,16 @@
+cd rollingball
+npm install
+cd ..
+sudo apt update
+sudo apt install fakeroot
+sudo apt install rpm
+sudo apt install ca-certificates gnupg
+sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo dpkg --add-architecture i386
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/focal/winehq-focal.sources
+sudo apt update
+sudo apt install mono-complete
+sudo apt install --install-recommends winehq-stable
